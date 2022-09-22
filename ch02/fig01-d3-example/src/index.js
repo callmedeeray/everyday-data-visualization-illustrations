7530a64ceb3f2c2562dc12ebf59f99210ba879e0
callmedeeray <deeray82@gmail.com>
2022-09-21 19:48:14 -0700

    Finished fig01-d3-example

--- ch02/fig01-d3-example/src/index.js
+++ ch02/fig01-d3-example/src/index.js
@@ -1,9 +1,9 @@
 
 import * as d3 from "d3";
-const tbl = require('url:./weekdayrentalsbyhour.csv');
+const tbl = require('url:./fig01-d3-example.csv');
 
 const width = 500,
-  height = 600;
+  height = 300;
 
 let viz = d3.select('#vizcontainer')
   .append('svg')
@@ -14,19 +14,14 @@ let viz = d3.select('#vizcontainer')
 
 d3.csv(tbl).then((data) => {
   const x = d3.scaleLinear()
-    .domain([0,1007000])
-    .range([width/4, width])
+    .domain([50,100])
+    .range([width/3, 2*width/3])
     ;
 
   const y = d3.scaleBand()
     .range([0,height])
-    .domain(data.map(d => d.hour))
+    .domain(data.map(d => d.Fruit))
     .padding(0.1);
-
-  const y2 = d3.scaleBand()
-      .range([0,height])
-      .domain(data.map(d => d3.format(',')(d.rentals)))
-      .padding(0.1);
 
   viz.append('g')
     .call(d3.axisLeft(y))
@@ -34,21 +29,16 @@ d3.csv(tbl).then((data) => {
     .attr('transform', 'translate(10,0)')
     ;
 
-  viz.append('g')
-    .call(d3.axisLeft(y2))
-    .selectAll('text')
-    .attr('transform', 'translate(50,0)')
-    ;
-
 
   viz.selectAll('rect')
     .data(data)
     .join('rect')
     .attr('x', x(0))
-    .attr('y', d => y(d.hour))
-    .attr('width', d => x(d.rentals))
+    .attr('y', d => y(d.Fruit))
+    .attr('width', d => x(d.Taste_score))
     .attr('height', y.bandwidth())
-    .attr('fill', 'grey')
+    .attr('transform', `translate(${width/3},0)`)
+    // .attr('fill', 'grey')
 
   viz.selectAll('.domain').remove();
   viz.selectAll('text')
@@ -59,7 +49,7 @@ d3.csv(tbl).then((data) => {
   ;
 
   let img = document.getElementById('#svg'),
-    filename = '2.4 Bar graph';
+    filename = 'd3 example';
     
   saveSvg(img, filename + '.svg');
 
