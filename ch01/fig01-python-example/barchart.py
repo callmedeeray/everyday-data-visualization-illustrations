3e64f5425130db56a802ed523efcd26021cf160d
callmedeeray <deeray82@gmail.com>
2022-11-16 17:46:43 -0800

    fix python bar chart

--- ch01/fig01-python-example/barchart.py
+++ ch01/fig01-python-example/barchart.py
@@ -3,22 +3,22 @@ import matplotlib.pyplot as plt
 data = {
     'Fuyu Persimmons': 100,
     'Mangos': 100,
-    'Peaches + Nectarines':100,
-    'Bananas':80,
-    'Kiwis':70,
-    'Apples':60,
-    'Oranges':50
+    'Peaches + Nectarines': 100,
+    'Bananas': 80,
+    'Kiwis': 70,
+    'Apples': 60,
+    'Oranges': 50
 }
 fruits = list(data.keys())
 taste = list(data.values())
 
-fig = plt.figure(figsize = (12,5))
+fig = plt.figure(figsize=(12, 5))
 plt.bar(fruits, taste)
-plt.ylabel('How tasty?')
+# plt.title('Taste score')
 plt.show()
 
 
-image_format = 'svg' # e.g .png, .svg, etc.
+image_format = 'svg'  # e.g .png, .svg, etc.
 image_name = 'fig01-python-example.svg'
 
 fig.savefig(image_name, format=image_format, dpi=1200)
