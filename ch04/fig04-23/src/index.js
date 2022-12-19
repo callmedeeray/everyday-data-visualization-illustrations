7cb003803c9bd10bccb774f401a317729c27d309
callmedeeray <deeray82@gmail.com>
2022-12-18 22:32:00 -0800

    two-hue

--- ch04/fig04-23/src/index.js
+++ ch04/fig04-23/src/index.js
@@ -2,27 +2,34 @@
 import * as d3 from "d3";
 
 const
-  width = 550,
-  height = 210
+  width = 700,
+  height = 300
   ;
 
-// const images = [
-//   { name: 'continuousSingleHue' },
-//   { name: 'singleVsTwoHue' },
-//   { name: 'colorSpaces' },
-// ];
 
-const imgName = 'continuousSingleHue';
-const data = Array.from(Array(100).keys());
+const imgName = 'single-vs-two-hue';
+let data = [];
+for (let i = 1; i <= 100; i++) {
+  data.push({ 'x': 10 + (width / 3 - 20) * Math.random(), 'y': 10 + (height - 20) * Math.random(), 'i': i })
+}
+
+
+
 
 let colorScale1 = d3.scaleSequential()
   .interpolator(d3.interpolate(d3.color('hsl(270,0%,50%)'), 'hsl(270,100%,50%)'))
   .domain([0, 99])
   ;
 
+
+let colorScale2 = d3.scaleSequential()
+  .interpolator(d3.interpolateHsl(d3.color('hsl(180,10%,70%)'), 'hsl(270,100%,50%)'))
+  .domain([1, 100])
+  ;
+
 let xScale = d3.scaleLinear()
-  .domain([0, 99])
-  .range([0, width]);
+  .domain([1, 100])
+  .range([0, (width / 3) - 20]);
 
 let viz = d3.select('#vizcontainer')
   .append('svg')
@@ -31,21 +38,56 @@ let viz = d3.select('#vizcontainer')
   .attr('height', height)
 
 let first = viz.append('g');
+
+first.selectAll('circle')
+  .data(data)
+  .join('circle')
+  .attr('r', 10)
+  .attr('cy', d => 0.8 * d.y)
+  .attr('cx', d => d.x)
+  .attr('fill', d => colorScale1(d.i))
+  .attr('stroke', 'none');
+
 first.selectAll('rect')
   .data(data)
   .join('rect')
-  .attr('height', 100)
-  .attr('y', 0)
-  .attr('x', d => Math.floor(xScale(d)))
+  .attr('x', d => 10 + Math.floor(xScale(d.i)))
+  .attr('y', 0.9 * height)
   .attr('width', (d) => {
-    if (d == 99.5) {
+    if (d.i == 100) {
       return 6;
     }
-    return Math.floor(xScale(d + 1)) - Math.floor(xScale(d)) + 1;
+    return Math.floor(xScale(d.i + 1)) - Math.floor(xScale(d.i)) + 1;
   })
-  .attr('fill', d => colorScale1(d));
+  .attr('height', 0.1 * height)
+  .attr('fill', d => colorScale1(d.i));
+
+let second = viz.append('g');
+second.selectAll('circle')
+  .data(data)
+  .join('circle')
+  .attr('r', 10)
+  .attr('cy', d => 0.8 * d.y)
+  .attr('cx', d => 310 + d.x)
+  .attr('fill', d => colorScale2(d.i))
+  .attr('stroke', 'none');
 
 
+
+
+second.selectAll('rect')
+  .data(data)
+  .join('rect')
+  .attr('x', d => 310 + Math.floor(xScale(d.i)))
+  .attr('y', 0.9 * height)
+  .attr('width', (d) => {
+    if (d.i == 100) {
+      return 6;
+    }
+    return Math.floor(xScale(d.i + 1)) - Math.floor(xScale(d.i)) + 1;
+  })
+  .attr('height', 0.1 * height)
+  .attr('fill', d => colorScale2(d.i))
 // let image = document.getElementById('#' + imgName);
 // saveSvg(image, imgName + '.svg');
 
