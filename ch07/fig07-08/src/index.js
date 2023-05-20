5fafd404a2905a27057a9e3ac87dd7166dcc5ae4
callmedeeray <deeray82@gmail.com>
2023-05-20 13:53:23 -0700

    Figure 8

--- ch07/fig07-08/src/index.js
+++ ch07/fig07-08/src/index.js
@@ -1,15 +1,13 @@
 
 import * as d3 from "d3";
-const tbl = require('url:./fruits.csv');
+const tbl = require('url:./weekdayrentalsbyhour.csv');
 
-const imgName = 'CH05_F16_Abbott-One-font';
+const imgName = 'CH07_F07_Abbott-Lines';
 
 
 const
-  margin = { top: 20, right: 20, bottom: 20, left: 20 },
-  width = 550 + margin.right + margin.left,
-  height = 300 + margin.top + margin.bottom,
-  titleSize = 16;
+  width = 360,
+  height = 840;
 
 
 let viz = d3.select('#vizcontainer')
@@ -17,70 +15,93 @@ let viz = d3.select('#vizcontainer')
   .attr('id', '#' + imgName)
   .attr('width', width)
   .attr('height', height)
-  .append('g')
-  .attr('transform', `translate(${margin.left},${margin.top})`)
-  ;
-
-viz.append('defs')
-  .append('style')
-  .attr('type', 'text/css')
-  .text("@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&family=Roboto+Serif:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700;8..144,800;8..144,900&family=Roboto:wght@100;300;400;500;700;900&display=swap')")
   ;
 
 d3.csv(tbl).then((data) => {
+
   const x = d3.scaleLinear()
-    .domain([0, 100])
-    .range([170, width - margin.left - margin.right - 30])
-    ;
+    .domain([0, 24])
+    .range([0.02 * width, 0.98 * width]);
 
-  const y = d3.scaleBand()
-    .domain(data.map(d => d.Fruit))
-    .range([(1.5 * titleSize) + margin.top, height - margin.bottom - margin.top])
-    .padding(0.25);
+  const multiplier = 2;
 
-  viz.append('g')
-    .call(d3.axisLeft(y))
-    .attr('transform', 'translate(10,0)') // I cannot figure out why I need this line but whatever.
-    .selectAll('text')
-    .style('font-family', 'Roboto, sans-serif')
-    .style('font-size', '16px')
-    .style('text-anchor', 'start')
-    ;
-
-  viz.append('g')
-    .selectAll('rect')
-    .data(data)
-    .join('rect')
-    .attr('x', x(0))
-    .attr('y', d => y(d.Fruit))
-    .attr('width', d => x(d.Taste_score) - x(0))
-    .attr('height', y.bandwidth())
-    .attr('fill', '#633091')
-    ;
+  const y1 = d3.scaleLinear()
+    .domain([0, multiplier * d3.max(data, d => d.value)])
+    .range([0.99 * height, 0.81 * height]);
+  const y2 = d3.scaleLinear()
+    .domain([0, multiplier * d3.max(data, d => d.value)])
+    .range([0.80 * height, 0.62 * height]);
+  const y3 = d3.scaleLinear()
+    .domain([0, multiplier * d3.max(data, d => d.value)])
+    .range([0.61 * height, 0.43 * height]);
+  const y4 = d3.scaleLinear()
+    .domain([0, multiplier * d3.max(data, d => d.value)])
+    .range([0.42 * height, 0.24 * height]);
+  const y5 = d3.scaleLinear()
+    .domain([0, multiplier * d3.max(data, d => d.value)])
+    .range([0.23 * height, 0.5 * height]);
 
   viz.append('g')
-    .selectAll('.label')
-    .data(data)
-    .join('text')
-    .text(d => d.Taste_score)
-    .attr('x', d => x(d.Taste_score) + 4)
-    .attr('y', d => y(d.Fruit) + 0.5 * y.bandwidth() + 5)
-    .style('font-family', 'Roboto, sans-serif')
-    .style('font-size', '16px')
-    .style('text-anchor', 'start')
-    ;
+    .append('path')
+    .datum(data)
+    .attr('fill', 'none')
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
+    .attr('d', d3.line()
+      .x(d => x(d.hour))
+      .y(d => y1(d.value * Math.random()))
+    );
 
-  viz.selectAll('.domain').remove();
-  viz.selectAll('.tick').select('line').remove();
+  viz.append('g')
+    .append('path')
+    .datum(data)
+    .attr('fill', 'none')
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
+    .attr('d', d3.line()
+      .x(d => x(d.hour))
+      .y(d => y2(d.value * Math.random()))
+    );
 
-  viz.append('text')
-    .text('Taste Scores of Fruits')
-    .attr('x', 0)
-    .attr('y', 0)
-    .attr('dominant-baseline', 'hanging')
-    .style('font-family', 'Roboto, sans-serif')
-    .style('font-size', `${titleSize}px`)
-    ;
+  viz.append('g')
+    .append('path')
+    .datum(data)
+    .attr('fill', 'none')
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
+    .attr('d', d3.line()
+      .x(d => x(d.hour))
+      .y(d => y3(d.value * Math.random()))
+    );
+
+  viz.append('g')
+    .append('path')
+    .datum(data)
+    .attr('fill', 'none')
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
+    .attr('d', d3.line()
+      .x(d => x(d.hour))
+      .y(d => y4(d.value * Math.random()))
+    );
+
+  viz.append('g')
+    .append('path')
+    .datum(data)
+    .attr('fill', 'none')
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
+    .attr('d', d3.line()
+      .x(d => x(d.hour))
+      .y(d => y5(d.value * Math.random()))
+    );
+
+
+
+
+
+
+
 
   // let image = document.getElementById('#' + imgName);
   // saveSvg(image, imgName.replace(/-/g, ' ') + '.svg');
